58ca412ad54aa6d91fdb7d1a687673ba4aad8ce8
elinelsonbrone <elinelsonbrone@gmail.com>
2024-05-05 00:26:38 -0300

    criando modal

--- script.js
+++ script.js
@@ -33,3 +33,18 @@ form.addEventListener('submit', function(event){
 });
 
 });
+
+
+const button = document.querySelector("button");
+const modal = document.querySelector("dialog");
+const buttonClose = document.querySelector("dialog button")
+button.onclick = function (){
+    modal.showModal()
+
+}
+
+buttonClose.onclick = function (){
+    modal.close()
+
+}
+
